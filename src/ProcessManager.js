81ba3e52f7ffe2a1a6b1b9e54519938c4e02ccbe
zkral <zbynek.kral@cern.ch>
2023-06-08 17:00:23 +0200

    Update ProcessManager.js

--- src/ProcessManager.js
+++ src/ProcessManager.js
@@ -1,8 +1,8 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import { Container, Row, Col, Button, Modal, Form, Table } from 'react-bootstrap';
 import { ProcessManagerClient } from './generated/process_manager_grpc_web_pb.js';
 import { Request } from './generated/request_response_pb';
-import { ProcessQuery, ProcessUUID, ProcessInstanceList, LogRequest, LogLine } from './generated/process_manager_pb';
+import { ProcessQuery, ProcessUUID, ProcessInstanceList, LogRequest, LogLine, BootRequest, ProcessDescription, ProcessMetadata, ProcessRestriction, ProcessInstance} from './generated/process_manager_pb';
 import { Token } from './generated/token_pb';
 import { Any } from 'google-protobuf/google/protobuf/any_pb';
 
@@ -17,6 +17,29 @@ function ProcessManager() {
   const [filter, setFilter] = useState({ uuid: '', user: '', session: '', name: '',isAlive: '', exitCode: '' });
   const [showModal, setShowModal] = useState(false);
   const [configFile, setConfigFile] = useState(null);
+  const [user, setUser] = useState('');
+  const [session, setSession] = useState('');
+  const [selectAll, setSelectAll] = useState(false);
+  const [allChecked, setAllChecked] = useState(false);
+
+  useEffect(() => {
+    if (processInstances.length === 0) {
+      setAllChecked(false);
+    } else if (selectedUUIDs.length === processInstances.length) {
+      setAllChecked(true);
+    } else {
+      setAllChecked(false);
+    }
+  }, [selectedUUIDs, processInstances]);
+
+  const handleSelectAllChange = useCallback((event) => {
+    setAllChecked(event.target.checked);
+    if (event.target.checked) {
+      setSelectedUUIDs(processInstances.map(processInstance => processInstance.getUuid().getUuid()));
+    } else {
+      setSelectedUUIDs([]);
+    }
+  }, [processInstances]);
 
   const client = useMemo(() => new ProcessManagerClient('http://localhost:8080', null, null), []);
   const token = useMemo(() => {
@@ -31,6 +54,65 @@ function ProcessManager() {
     return r;
   }, [token]);
 
+  const boot = useCallback(async (bootConfigurationFile, user, session) => {
+    // Create a new FileReader object
+    const reader = new FileReader();
+  
+    // Use a Promise to handle the asynchronous read operation
+    const fileContents = await new Promise((resolve, reject) => {
+      reader.onload = (event) => resolve(event.target.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsText(bootConfigurationFile);
+    });
+  
+    const bootConfiguration = JSON.parse(fileContents);
+  
+    for (const app of bootConfiguration.instances) {
+      const executableAndArguments = [];
+      for (const execargs of bootConfiguration.executables[app.type].executable_and_arguments) {
+        for (const [exe, args] of Object.entries(execargs)) {
+          const execAndArgs = new ProcessDescription.ExecAndArgs();
+          execAndArgs.setExec(exe);
+          execAndArgs.setArgsList(args);
+          executableAndArguments.push(execAndArgs);
+        }
+      }
+  
+      const bootRequest = new BootRequest();
+      const processDescription = new ProcessDescription();
+      const processMetadata = new ProcessMetadata();
+      const processRestriction = new ProcessRestriction();
+  
+      processMetadata.setUser(user);
+      processMetadata.setSession(session);
+      processMetadata.setName(app.name);
+  
+      processDescription.setMetadata(processMetadata);
+      processDescription.setExecutableAndArgumentsList(executableAndArguments);
+  
+      processRestriction.setAllowedHostsList(bootConfiguration.restrictions[app.restriction].hosts);
+  
+      bootRequest.setProcessDescription(processDescription);
+      bootRequest.setProcessRestriction(processRestriction);
+  
+      const any = new Any();
+      any.pack(bootRequest.serializeBinary(), "DUNEProcessManager.BootRequest");
+      request.setData(any);
+  
+      client.boot(request, {}, (error, response) => {
+        if (error) {
+          logError(error);
+          return;
+        }
+  
+        const processInstance = response.getData().unpack(ProcessInstance.deserializeBinary, 'DUNEProcessManager.ProcessInstance');
+        console.log('ProcessInstance:', processInstance);
+        // Handle the ProcessInstance here
+      });
+    }
+  }, [client, request]);
+
+
   const handleKill = useCallback(() => {
     const query = new ProcessQuery();
     const any = new Any();
@@ -44,6 +126,30 @@ function ProcessManager() {
       request.setData(any);
   
       client.kill(request, {}, (error, response) => {
+        if (error) {
+          logError(error)
+          return;
+        }
+  
+        console.log('Response:', response);
+        // Handle the response
+      });
+    });
+  }, [selectedUUIDs, client, request]);
+
+  const handleFlush = useCallback(() => {
+    const query = new ProcessQuery();
+    const any = new Any();
+  
+    selectedUUIDs.forEach(uuid => {
+      const processUUID = new ProcessUUID();
+      processUUID.setUuid(uuid);
+      query.setUuid(processUUID);
+  
+      any.pack(query.serializeBinary(), "DUNEProcessManager.ProcessQuery");
+      request.setData(any);
+  
+      client.flush(request, {}, (error, response) => {
         if (error) {
           logError(error)
           return;
@@ -79,6 +185,7 @@ function ProcessManager() {
       console.log('Unpacked response:', processInstanceList.getValuesList());
       setProcessInstances(processInstanceList.getValuesList());
     });
+    setSelectAll(false);
   }, [client, request]);
 
   const fetchLogs = useCallback(() => {
@@ -128,17 +235,20 @@ processUUID.setUuid(selectedUUIDs[0]);
       setShowModal(false);
       console.log(configFile)
       console.log(request)
-      // Here you can call your boot function with the selected file
+      boot(configFile, user, session)
       console.log('Boot with configuration file:', configFile.name);
       
     } else if (action === 'ps') {
+      setSelectedUUIDs([]);
       ps();
     } else if (action === 'kill') {
       handleKill();
     } else if (action === 'logs') {
       fetchLogs();
+    }  else if (action === 'flush') {
+      handleFlush();
     } 
-  }, [handleKill, ps, fetchLogs, client, request, configFile]);
+  }, [handleKill, ps, fetchLogs, client, request, configFile, user, session]);
 
   const handleFilterChange = useCallback((event, field) => {
     setFilter({ ...filter, [field]: event.target.value });
@@ -162,11 +272,19 @@ processUUID.setUuid(selectedUUIDs[0]);
 
   const handleCheckboxChange = useCallback((event, uuid) => {
     if (event.target.checked) {
-      setSelectedUUIDs([...selectedUUIDs, uuid]);
+      setSelectedUUIDs(prevUUIDs => {
+        const newUUIDs = [...prevUUIDs, uuid];
+        // Check if all checkboxes are checked
+        if (newUUIDs.length === processInstances.length) {
+          setSelectAll(true);
+        }
+        return newUUIDs;
+      });
     } else {
-      setSelectedUUIDs(selectedUUIDs.filter(item => item !== uuid));
+      setSelectAll(false);
+      setSelectedUUIDs(prevUUIDs => prevUUIDs.filter(item => item !== uuid));
     }
-  }, [selectedUUIDs]);
+  }, [processInstances]);
 
   const handleModalClose = useCallback(() => {
     // Reset modal state on close
@@ -195,29 +313,38 @@ processUUID.setUuid(selectedUUIDs[0]);
           <Button variant="secondary" onClick={() => handleActionClick('kill')}>Kill</Button>{' '}
         </Col>
         <Col>
-          <Button variant="secondary" onClick={() => handleActionClick('restart')}>Flush</Button>{' '}
+          <Button variant="secondary" onClick={() => handleActionClick('flush')}>Flush</Button>{' '}
+          <Button variant="secondary" onClick={() => handleActionClick('restart')}>Restart</Button>{' '}
           <Button variant="secondary" onClick={() => handleActionClick('ps')}>PS</Button>{' '}
         </Col>
       </Row>
       <Modal show={showModal} onHide={handleModalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Boot Options</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Group>
-            <Form.Label>Select boot configuration file:</Form.Label>
-            <Form.Control type="file" onChange={handleFileChange} />
-          </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="dark" onClick={handleModalClose}>
-            Close
-          </Button>
-          <Button variant="secondary" onClick={() => handleActionClick('boot')}>
-            Boot
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <Modal.Header closeButton>
+        <Modal.Title>Boot Options</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+  <Form.Group>
+    <Form.Label>Select boot configuration file:</Form.Label>
+    <Form.Control type="file" onChange={handleFileChange} />
+  </Form.Group>
+  <Form.Group>
+    <Form.Label>User:</Form.Label>
+    <Form.Control type="text" value={user} onChange={(e) => setUser(e.target.value)} />
+  </Form.Group>
+  <Form.Group>
+    <Form.Label>Session:</Form.Label>
+    <Form.Control type="text" value={session} onChange={(e) => setSession(e.target.value)} />
+  </Form.Group>
+</Modal.Body>
+      <Modal.Footer>
+        <Button variant="dark" onClick={handleModalClose}>
+          Close
+        </Button>
+        <Button variant="secondary" onClick={() => handleActionClick('boot')}>
+          Boot
+        </Button>
+      </Modal.Footer>
+    </Modal>
       <Row>
         <Col>
           <h1>Process List</h1>
@@ -273,7 +400,15 @@ processUUID.setUuid(selectedUUIDs[0]);
                   onChange={(e) => handleFilterChange(e, 'exitCode')}
                   />
                 </th>
-                <th></th>
+                <th>
+                <Form.Check
+                  type="checkbox"
+                  id="select-all-checkbox"
+                  label=""
+                  checked={allChecked}
+                  onChange={handleSelectAllChange}
+                />
+              </th>
               </tr>
             </thead>
             <tbody>
@@ -287,10 +422,11 @@ processUUID.setUuid(selectedUUIDs[0]);
               <td>{processInstance.getStatusCode() === 0 ? "Yes" : "No"}</td>
 <td>{processInstance.getReturnCode() ? processInstance.getReturnCode().toString() : ''}</td>
               <td>
-                <Form.Check
+              <Form.Check
                   type="checkbox"
                   id={`default-checkbox-${index}`}
                   label=""
+                  checked={selectedUUIDs.includes(processInstance.getUuid().getUuid())}
                   onChange={(event) => handleCheckboxChange(event, processInstance.getUuid().getUuid())}
                 />
               </td>
