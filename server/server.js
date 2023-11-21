7a2f6e9f82a70dfbb9726849847a3e2892658717
zkral <zbynek.kral@cern.ch>
2023-11-21 13:13:42 +0100

    Broadcast decoding

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const { Kafka } = require('kafkajs');
 const WebSocket = require('ws');
 const http = require('http');
+const jspb = require('google-protobuf');
+const proto = require('../src/generated/generic_pb.js');
+const broadcastProto = require('../src/generated/broadcast_pb'); // Replace with the correct path to broadcast_pb.js
 
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
@@ -12,18 +15,38 @@ const kafka = new Kafka({
 
 const consumer = kafka.consumer({ groupId: 'my-group' });
 
+// Deserialize function for BroadcastMessage
+function deserializeBroadcastMessage(binaryData) {
+  var broadcastMessage = broadcastProto.BroadcastMessage.deserializeBinary(binaryData);
+  var anyData = broadcastMessage.getData();
+  var plainTextMessage = new proto.PlainText();
+  console.log(anyData.getValue())
+  if (anyData.getTypeUrl().endsWith('dunedaq.druncschema.PlainText')) {
+    var plainTextMessage = proto.PlainText.deserializeBinary(anyData.getValue());
+    return plainTextMessage.getText();
+  } else {
+    throw new Error("Data does not contain a PlainText message");
+  }
+  
+}
+
 const start = async () => {
   await consumer.connect();
   await consumer.subscribe({ topic: 'process_manager.no_session', fromBeginning: true });
 
   consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      const payload = message.value.toString();
-      wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(payload);
-        }
-      });
+      console.log(message.value)
+      try {
+        const text = deserializeBroadcastMessage(message.value);
+        wss.clients.forEach(client => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(text);
+          }
+        });
+      } catch (error) {
+        console.error("Error deserializing message:", error);
+      }
     },
   });
 
--- src/ConsoleWindow.js
+++ src/ConsoleWindow.js
@@ -6,32 +6,41 @@ const ConsoleWindow = ({ onClose }) => {
 
   useEffect(() => {
     // Open a new window
-    externalWindow = window.open('', '', 'width=600,height=400,left=200,top=200');
+    externalWindow = window.open('', '', 'width=800,height=400,left=200,top=200,location=no,menubar=no,status=no,toolbar=no');
     if (!externalWindow) {
       console.error('Failed to open the new window');
       return;
     }
 
-    // Write initial HTML structure to the new window with terminal-like styles
+    // Write initial HTML structure to the new window with inline styles that resemble React Bootstrap
     externalWindow.document.write(`
       <html>
         <head>
           <title>Kafka Stream Console</title>
           <style>
-            body { 
-              font-family: 'Courier New', monospace; 
+            body {
+              font-family: 'Lucida Console', Monaco, monospace;
               margin: 0;
               height: 100%;
-              overflow: hidden; 
+              overflow: hidden;
+              background-color: #212529;
+              color: #f8f9fa;
             }
-            #console { 
-              background-color: #000; 
-              color: #33FF00; 
-              padding: 10px;
-              height: calc(100% - 20px);
+            #console {
+              padding: 0.5rem;
+              height: 100%;
+              overflow-y: auto;
               white-space: pre-wrap;
-              overflow-y: scroll;
               box-sizing: border-box;
+            }
+            .message {
+              white-space: pre-wrap; /* Allow text to wrap */
+              overflow-wrap: break-word; /* Breaks the word to prevent overflow */
+              margin-bottom: 5px;
+            }
+            .timestamp {
+              color: #b2b2b2; /* Slightly lighter color for the timestamp */
+              margin-right: 5px; /* Space between timestamp and message */
             }
           </style>
         </head>
@@ -67,13 +76,24 @@ const ConsoleWindow = ({ onClose }) => {
 
     ws.onmessage = (event) => {
       const message = event.data;
+      const timestamp = new Date().toISOString().replace('T', ' ').substr(0, 19); // ISO format without 'T' and milliseconds
       console.log('Received:', message);
-      setMessages(prevMessages => [...prevMessages, message]);
-      // Update the content of the new window
+      setMessages(prevMessages => [...prevMessages, { timestamp, message }]);
+
       if (externalWindow && !externalWindow.closed) {
         const consoleDiv = externalWindow.document.getElementById('console');
-        consoleDiv.textContent += `${message}\n`; // Add the new message to the console
-        consoleDiv.scrollTop = consoleDiv.scrollHeight; // Auto-scroll to the latest message
+        const messageDiv = externalWindow.document.createElement('div');
+        messageDiv.className = 'message';
+        // Append timestamp and message as separate elements for styling
+        const timestampSpan = externalWindow.document.createElement('span');
+        timestampSpan.className = 'timestamp';
+        timestampSpan.textContent = `[${timestamp}] `;
+        const messageTextSpan = externalWindow.document.createElement('span');
+        messageTextSpan.textContent = message;
+        messageDiv.appendChild(timestampSpan);
+        messageDiv.appendChild(messageTextSpan);
+        consoleDiv.appendChild(messageDiv);
+        consoleDiv.scrollTop = consoleDiv.scrollHeight;
       }
     };
 
